2d00e7aaaf057fe21fb17c60923d3d0688623f33
Hector Castro <hectcastro@gmail.com>
2017-10-03 08:06:32 -0400

    Add documentation
    Document the purpose of each function.

--- src/Runner.hs
+++ src/Runner.hs
@@ -9,6 +9,8 @@ import           System.Exit
 import           System.FilePath
 import           System.Process
 
+-- |Runs another program with the environment modified
+-- according to files in a specified directory.
 run :: FilePath -> [String] -> IO (ExitCode, String, String)
 run _ [] = fail "usage: envdir dir child"
 run d (x:xs) = do
@@ -19,16 +21,22 @@ run d (x:xs) = do
   readCreateProcessWithExitCode (proc x xs){env = Just newEnv} ""
   where fs = environmentFiles d
 
+-- |Collects files in a directory and prepends the directory
+-- path to each.
 environmentFiles :: FilePath -> IO [FilePath]
 environmentFiles d = filterM doesFileExist =<< l
   where l = map (d </>) <$> listDirectory d
 
+-- |Reads the contents of a list of FilePaths and returns
+-- a list of their contents trimmed of whitespace.
 getEnvironmentFileContents :: IO [FilePath] -> IO [String]
 getEnvironmentFileContents xs = mapM f =<< xs
   where f x = do
           v <- readFile x
           return $ T.unpack . T.strip . T.pack $ v
 
+-- |Extracts the basename of a list of FilePaths and returns
+-- it as a list.
 getEnvironmentFileBaseName :: IO [FilePath] -> IO [String]
 getEnvironmentFileBaseName xs = do
   fps <- xs
